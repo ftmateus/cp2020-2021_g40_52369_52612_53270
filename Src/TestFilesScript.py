3c8608234e55a8abc1907038149410c60bcaa576
Dionisie <d.tarlev@campus.fct.unl.pt>
2021-06-04 23:08:46 +0100

    Updated README.md

--- Src/TestFilesScript.py
+++ Src/TestFilesScript.py
@@ -18,19 +18,19 @@ MAX_THREADS = os.cpu_count()
 # samples = run_tests(layer_size, all_test_files, n_runs = 5)
 
 #testing files individualy
-#for test in all_test_files:
-#    print("Using test file:", test)
-#    run_tests(layer_size, [test], n_runs = N_RUNS)#
-#
+for test in all_test_files:
+   print("Using test file:", test)
+   run_tests(layer_size, [test], n_runs = N_RUNS)#
+
 
 #print("Using all test_01_* files")
-#run_tests(layer_size, get_test_files("test_01_*"), n_runs = N_RUNS)
+run_tests(layer_size, get_test_files("test_01_*"), n_runs = N_RUNS)
 #
 #print("Using all test_02_* files")
-#run_tests(layer_size, get_test_files("test_02_*"), n_runs = N_RUNS)
+run_tests(layer_size, get_test_files("test_02_*"), n_runs = N_RUNS)
 
 # print("Using all test_07_* files")
-# run_tests(layer_size, get_test_files("test_07_*"), n_runs = N_RUNS)
+run_tests(layer_size, get_test_files("test_07_*"), n_runs = N_RUNS)
 
 print("Using all test_08_* files")
 run_tests(layer_size, get_test_files("test_08_*"), n_runs = N_RUNS)
